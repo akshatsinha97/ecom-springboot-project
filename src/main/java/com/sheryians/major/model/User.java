0e6b0be9db37c9cfeb30b8a97b5c00e52eefe9aa
akshat <akshatsinha97@gmail.com>
2022-11-03 04:34:51 +0530

    added homeController & user model

---
+++ src/main/java/com/sheryians/major/model/User.java
@@ -0,0 +1,34 @@
+package com.sheryians.major.model;
+
+import lombok.Data;
+
+import javax.management.relation.Role;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotEmpty
+    @Column(nullable = false)
+    private String firstName;
+
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    @NotEmpty
+    @Email(message = "{errors.invalid_email}")
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    @ManyToMany
+    private List<Role> roles;
+}
