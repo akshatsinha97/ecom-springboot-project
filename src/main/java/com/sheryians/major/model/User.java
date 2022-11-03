cc957ab4e582f1fafbdfdce2d072dcdd6338c1c3
akshat <akshatsinha97@gmail.com>
2022-11-04 03:00:30 +0530

    customUserDetail model created.

--- src/main/java/com/sheryians/major/model/User.java
+++ src/main/java/com/sheryians/major/model/User.java
@@ -6,6 +6,7 @@ import javax.management.relation.Role;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
 
 @Entity
 @Data
@@ -29,6 +30,22 @@ public class User {
     @NotEmpty
     private String password;
 
-    @ManyToMany
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
+                inverseJoinColumns = {@JoinColumn(name = "Role_ID", referencedColumnName = "ID")}
+    )
     private List<Role> roles;
+
+    public User(User user) {
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.roles = user.getRoles();
+    }
+
+    public User(){
+    }
 }
